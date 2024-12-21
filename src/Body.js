442ed25526a86678f081ab86eb1f24de485093ec
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2024-12-21 12:22:57 +0500

    Header

---
+++ src/Body.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Body.css";
+
+const Body = () => {
+  return (
+    <main className="body">
+      <h2>Welcome to the Body Section</h2>
+      <p>This is the content of the body component.</p>
+    </main>
+  );
+};
+
+export default Body;
+
+
