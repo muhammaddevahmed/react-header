fc7b5437babc12a85228a528bb98a7de81c1deb9
Muhammad Ahmed <m.ahmed.uh72@gmail.com>
2024-12-21 12:29:58 +0500

    Header

--- src/Body.js
+++ src/Body.js
@@ -6,6 +6,9 @@ const Body = () => {
     <main className="body">
       <h2>Welcome to the Body Section</h2>
       <p>This is the content of the body component.</p>
+      <p>
+        I am Muhammad Ahmed Professional Front-end Developer
+      </p>
     </main>
   );
 };
